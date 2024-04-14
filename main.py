dd6a7a7a67ba13464446127b00def75edf8baf64
Zion Hall <marioandluigifanatic@yahoo.com>
2024-04-14 18:46:32 -0500

    Update main.py
    Added proper TKinter UI and progressbar

--- main.py
+++ main.py
@@ -2,87 +2,88 @@ from pytube import YouTube
 from pytube import Playlist
 import os
 from tkinter import *
+import tkinter as tk
+from tkinter import filedialog, messagebox, ttk
 import moviepy.editor as mp
 import re
 
 
-from tkinter import *
+
+output_folder = ""
+
+folder_selected = False
  
 root = Tk()
-root.geometry("300x300")
 root.title(" YouTube Playlist Downloader ")
  
 def StartConversion():
+    print(folder_selected)
     PLAYLISTINPUT = playlisttxt.get("1.0", "end-1c")
-    ALBUMINPUT = albumtxt.get("1.0", "end-1c")
-    print(PLAYLISTINPUT, ALBUMINPUT)
-
-    current_folder = os.getcwd()
-    #print(current_folder)
 
     playlist_link_input = PLAYLISTINPUT
     playlist = Playlist(playlist_link_input)
 
-    #Lets user name the folder for the music.
+    if playlist_link_input == "":
+        messagebox.showerror("Error", "Please enter a playlist URL")
+        return
 
-    new_album_path = str(fr"{current_folder}\{ALBUMINPUT}")
-    #Turns the folder name and its path into a string.
+    elif folder_selected == False:
+        messagebox.showerror("Error", "Please select an output folder")
+        return
 
-    isdir = os.path.isdir(new_album_path)
-    #Checks if the folder already exists
+    n = 1
+    for i, url in enumerate(playlist):
+        total_videos = len(playlist.video_urls)
+        progress_step = 100 / total_videos
+        YouTube(url).streams.filter(only_audio=True).first().download(output_folder)
+        for file in os.listdir(output_folder):
+            if re.search('mp4', file):
+                mp4_path = os.path.join(output_folder, file)
+                mp3_path = os.path.join(output_folder, os.path.splitext(file)[0]+'.mp3')
+                new_file = mp.AudioFileClip(mp4_path)
+                new_file.write_audiofile(mp3_path)
+                os.remove(mp4_path)
+                n + 1
+                progress.set(progress.get() + progress_step)
+                progressbar.update()
 
-    if isdir == False:
-        os.mkdir(ALBUMINPUT)
-        folder = new_album_path
-    else:
-        ALBUMINPUT = input("Sorry. That name is taken. Try a new name:")
+            
+                      
+            
 
-    #Saves the path to wherever the current directory is as a string variable.
-
-    #prints each video url, which is the same as iterating through playlist.video_urls
-    for url in playlist:
-        print(url)
-    #prints address of each YouTube object in the playlist
-    for vid in playlist.videos:
-        print(vid)
-
-    for url in playlist:
-        YouTube(url).streams.filter(only_audio=True).first().download(folder)
-
-    for url in playlist:
-        YouTube(url).streams.first().download()
-
-    for file in os.listdir(folder):
-        if re.search('mp4', file):
-            mp4_path = os.path.join(folder,file)
-            mp3_path = os.path.join(folder,os.path.splitext(file)[0]+'.mp3')
-            new_file = mp.AudioFileClip(mp4_path)
-            new_file.write_audiofile(mp3_path)
-            os.remove(mp4_path)
+def select_folder():
+        global output_folder
+        output_folder = filedialog.askdirectory()
+        global folder_selected
+        folder_selected = True
+        print(folder_selected)
      
 l1 = Label(text = "Enter your playlist link:")
-playlisttxt = Text(root, height = 5,
-                width = 25,
-                bg = "light yellow")
-
-l2 = Label(text = "Enter your album name:")
-albumtxt = Text(root, height = 5,
+playlisttxt = Text(root, height = 1,
                 width = 25,
                 bg = "light yellow")
                 
- 
-Display = Button(root, height = 2,
+
+folder_button = Button(root, height = 2,
                  width = 20,
-                 text= "Start Conversion", 
+                 text= "Select Folder", 
+                 command = lambda:select_folder())
+
+download_button = Button(root, height = 2,
+                 width = 20,
+                 text= "Download", 
                  command = lambda:StartConversion())
-   
+
+progress = tk.DoubleVar()
+progressbar = ttk.Progressbar(variable=progress, maximum=100)
+progressbar.place(x=50, y=250, width=200)
+progress.set(0)
 
 
 l1.pack()
 playlisttxt.pack()
-l2.pack()
-albumtxt.pack()
-Display.pack()
+folder_button.pack()
+download_button.pack()
+progressbar.pack(fill=tk.X, padx=10, pady=5)
  
 mainloop()
-
