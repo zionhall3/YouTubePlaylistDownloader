be51a1b865bcb0e2e239a51d8965862f24863fda
Zion Hall <marioandluigifanatic@yahoo.com>
2024-07-04 19:49:44 -0500

    Single Download Functionality

--- main.py
+++ main.py
@@ -36,57 +36,84 @@ def StartConversion():
         messagebox.showerror("Error", "Please select an output folder")
         return
     #Returns errors to make sure folders are selected and there is a link.
-
-    for i, url in enumerate(playlist, 1):
-        total_videos = len(playlist.video_urls)
-        progress_step = 100 / total_videos
-        video = YouTube(url)
-        vid_id = extract.video_id(url)
-        thumbnail = Thumbnail(f"https://youtu.be/{vid_id}")
-        video.streams.filter(only_audio=True).first().download(output_folder)
-        for file in os.listdir(output_folder):
-            if re.search('mp4', file):
-                progress.set(progress.get() + progress_step)
-                thumbnail.fetch(size="maxresdefault")
-                thumbnail.save(output_folder, "album_art_file", overwrite=True)
-                album_art_path = os.path.join(output_folder, "album_art_file.jpg")
-                mp4_path = os.path.join(output_folder, file)
-                mp3_path = os.path.join(output_folder, os.path.splitext(file)[0]+'.mp3')
-                new_file = mp.AudioFileClip(mp4_path)
-                new_file.write_audiofile(mp3_path)
-                mp3_filename = os.path.basename(mp3_path)
-                selected_music_file = os.path.join(output_folder, mp3_filename)
-                no_art_file = eyed3.load(selected_music_file)
-                no_art_file.tag.images.set(3, open(album_art_path, 'rb').read(), 'image/jpg')
-                no_art_file.tag.save()
-                if i < 10:
-                     original_file_path = os.path.join(output_folder, mp3_filename)
-                     numbered_file_name = f"00{i}_{mp3_filename}"
-                     numbered_file_path = os.path.join(output_folder, numbered_file_name)
-                     if os.path.exists(numbered_file_path):
-                          os.remove(numbered_file_path)
-                     os.rename(original_file_path, numbered_file_path)
-                elif 100 > i >= 10:
-                     original_file_path = os.path.join(output_folder, mp3_filename)
-                     numbered_file_name = f"0{i}_{mp3_filename}"
-                     numbered_file_path = os.path.join(output_folder, numbered_file_name)
-                     if os.path.exists(numbered_file_path):
-                          os.remove(numbered_file_path)
-                     os.rename(original_file_path, numbered_file_path)
-                else:
-                     original_file_path = os.path.join(output_folder, mp3_filename)
-                     numbered_file_name = f"{i}_{mp3_filename}"
-                     numbered_file_path = os.path.join(output_folder, numbered_file_name)
-                     if os.path.exists(numbered_file_path):
-                          os.remove(numbered_file_path)
-                     os.rename(original_file_path, numbered_file_path)
-                os.remove(mp4_path)
-                os.remove(album_art_path)
-                if progressbar.step(99.9):
-                     messagebox.showinfo("Finished!", "The playlist is done downlading and converting!")
-                     os.startfile(output_folder)
-                     return
-                progressbar.update()
+    
+    if "youtube.com/watch?v=" in playlist_link_input:
+         video = YouTube(playlist_link_input)
+         vid_id = extract.video_id(playlist_link_input)
+         thumbnail = Thumbnail(f"https://youtu.be/{vid_id}")
+         video.streams.filter(only_audio=True).first().download(output_folder)
+         thumbnail.fetch(size="maxresdefault")
+         thumbnail.save(output_folder, "album_art_file", overwrite=True)
+         album_art_path = os.path.join(output_folder, "album_art_file.jpg")
+         mp4_path = os.path.join(output_folder, f"{video.title}.mp4")
+         mp3_path = os.path.join(output_folder, os.path.splitext(video.title)[0]+'.mp3')
+         new_file = mp.AudioFileClip(mp4_path)
+         new_file.write_audiofile(mp3_path)
+         mp3_filename = os.path.basename(mp3_path)
+         selected_music_file = os.path.join(output_folder, mp3_filename)
+         no_art_file = eyed3.load(selected_music_file)
+         no_art_file.tag.images.set(3, open(album_art_path, 'rb').read(), 'image/jpg')
+         no_art_file.tag.save()
+         os.remove(mp4_path)
+         os.remove(album_art_path)
+         progress.set(100)
+         if progressbar.step(100):
+          messagebox.showinfo("Finished!", "The song is done downlading and converting!")
+          os.startfile(output_folder)
+          return
+         progressbar.update()
+    
+    else:
+     for i, url in enumerate(playlist, 1):
+          total_videos = len(playlist.video_urls)
+          progress_step = 100 / total_videos
+          video = YouTube(url)
+          vid_id = extract.video_id(url)
+          thumbnail = Thumbnail(f"https://youtu.be/{vid_id}")
+          video.streams.filter(only_audio=True).first().download(output_folder)
+          for file in os.listdir(output_folder):
+               if re.search('mp4', file):
+                    progress.set(progress.get() + progress_step)
+                    thumbnail.fetch(size="maxresdefault")
+                    thumbnail.save(output_folder, "album_art_file", overwrite=True)
+                    album_art_path = os.path.join(output_folder, "album_art_file.jpg")
+                    mp4_path = os.path.join(output_folder, file)
+                    mp3_path = os.path.join(output_folder, os.path.splitext(file)[0]+'.mp3')
+                    new_file = mp.AudioFileClip(mp4_path)
+                    new_file.write_audiofile(mp3_path)
+                    mp3_filename = os.path.basename(mp3_path)
+                    selected_music_file = os.path.join(output_folder, mp3_filename)
+                    no_art_file = eyed3.load(selected_music_file)
+                    no_art_file.tag.images.set(3, open(album_art_path, 'rb').read(), 'image/jpg')
+                    no_art_file.tag.save()
+                    if i < 10:
+                         original_file_path = os.path.join(output_folder, mp3_filename)
+                         numbered_file_name = f"00{i}_{mp3_filename}"
+                         numbered_file_path = os.path.join(output_folder, numbered_file_name)
+                         if os.path.exists(numbered_file_path):
+                              os.remove(numbered_file_path)
+                         os.rename(original_file_path, numbered_file_path)
+                    elif 100 > i >= 10:
+                         original_file_path = os.path.join(output_folder, mp3_filename)
+                         numbered_file_name = f"0{i}_{mp3_filename}"
+                         numbered_file_path = os.path.join(output_folder, numbered_file_name)
+                         if os.path.exists(numbered_file_path):
+                              os.remove(numbered_file_path)
+                         os.rename(original_file_path, numbered_file_path)
+                    else:
+                         original_file_path = os.path.join(output_folder, mp3_filename)
+                         numbered_file_name = f"{i}_{mp3_filename}"
+                         numbered_file_path = os.path.join(output_folder, numbered_file_name)
+                         if os.path.exists(numbered_file_path):
+                              os.remove(numbered_file_path)
+                         os.rename(original_file_path, numbered_file_path)
+                    os.remove(mp4_path)
+                    os.remove(album_art_path)
+                    if progressbar.step(99.9):
+                         messagebox.showinfo("Finished!", "The playlist is done downlading and converting!")
+                         os.startfile(output_folder)
+                         return
+                    progressbar.update()
 
 
 Folder_Button_Text1 = "Please select a folder"
